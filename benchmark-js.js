1ee9996170627d2f27b3f5d0bb4ea938fb5d59ff
zhanghengxin <ezreal.zhang@icewhale.org>
2023-09-19 15:43:32 +0800

    wat 文本格式

--- benchmark-js.js
+++ benchmark-js.js
@@ -1,11 +1,12 @@
 /*
- * @Author: zhanghengxin 812718649@qq.com
- * @Date: 2023-09-17 13:37:51
- * @LastEditors: zhanghengxin 812718649@qq.com
- * @LastEditTime: 2023-09-17 13:43:40
- * @FilePath: \webAssembly\benchmark-js.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Author: zhanghengxin ezreal.zhang@icewhale.org
+ * @Date: 2023-09-19 09:56:18
+ * @LastEditTime: 2023-09-19 10:17:52
+ * @LastEditors: zhanghengxin ezreal.zhang@icewhale.org
+ * @FilePath: /webAssembly/benchmark-js.js
+ * @Description: Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
  */
+
 (function(){
     let a = 6.2456, b = 2.178;
     const start = performance.now();
@@ -20,7 +21,7 @@
     const end = performance.now();
     console.log(`${end - start} ms`)
     return 0;
-})()
+})();
 
 // total:
 // 1346ms
