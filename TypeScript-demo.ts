f7d6ba07e0ee235f52a5065c75c4d43374a3601f
zhanghengxin <ezreal.zhang@icewhale.org>
2023-09-19 17:00:33 +0800

    assemblyScript

---
+++ TypeScript-demo.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: zhanghengxin ezreal.zhang@icewhale.org
+ * @Date: 2023-09-19 16:08:58
+ * @LastEditTime: 2023-09-19 16:32:07
+ * @LastEditors: zhanghengxin ezreal.zhang@icewhale.org
+ * @FilePath: /webAssembly/TypeScript-demo.ts
+ * @Description: Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+export function addTwo(a: u32, b: u32): u32 {
+  return a + b;
+}
