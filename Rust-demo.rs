d90a7d00482058ba4d3530a284aa0441fe8a8915
zhanghengxin <ezreal.zhang@icewhale.org>
2023-09-20 22:56:13 +0800

    wip

---
+++ Rust-demo.rs
@@ -0,0 +1,11 @@
+/*
+ * @Author: zhanghengxin ezreal.zhang@icewhale.org
+ * @Date: 2023-09-19 19:17:50
+ * @LastEditTime: 2023-09-19 19:18:22
+ * @LastEditors: zhanghengxin ezreal.zhang@icewhale.org
+ * @FilePath: /webAssembly/Rust-demo.rs
+ * @Description: Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
+pub fn add(left: i32, right: i32) -> i32 {
+    left + right
+}
